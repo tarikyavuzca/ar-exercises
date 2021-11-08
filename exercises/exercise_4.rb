bef83b12d3f22d6be97745e25b0d125ddbf5a187
tarikyavuzca <tarikyavuzca@gmail.com>
2021-11-08 16:22:27 -0500

    creating 3 more store, fething data using where clause and display them by looping through

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,20 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create([
+  {name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true},
+  {name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false},
+  {name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true}
+])
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each {|store| 
+  puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}" 
+  }
+
+@womens_stores_under_1m = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
+@womens_stores_under_1m.each {|store| 
+  puts "Name: #{store.name}, Annual Revenue: #{store.annual_revenue}" 
+  }
